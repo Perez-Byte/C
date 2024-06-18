dfe85238177071cd8f1f6510071b9e17c6c0b353
Igor Perestoronin <perez_@Igors-MacBook-Air.local>
2024-06-18 12:30:33 +0300

    Second task CS50

--- read.c
+++ read.c
@@ -1,17 +1,49 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int sec(char * word) {
-	int s = 0;
+int countWords(char *word) {
+	int w = 0;
 	for(int i = 0; i < strlen(word); i++) {
 		if(word[i] == ' ') {
+			w++;
+		}
+	}
+	return w + 1;
+}
+
+int countSentences(char *sentence) {
+	int s = 0;
+	for(int i = 0; i < strlen(sentence); i++) {
+		if(sentence[i] == '.' || sentence[i] == '!' || sentence[i] == '?' ) {
 			s++;
 		}
 	}
-	return s + 1;
+	return s;
+}
+
+int countLetters(char *letter) {
+	int l = 0;
+	for(int i = 0; i < strlen(letter); i++) {
+		if(isalpha(letter[i])) {
+			l++;
+		}
+	}
+	return l;
 }
 
 int main(void) {
-	char *a = "This was CS50";
-	printf("%d\n", sec(a));
+	char *a = "Congratulations! Today is your day. You're off to Great Places! You're off and away!";
+	float L = (float) countLetters(a) / (float)  countWords(a) * 100;
+	float S = (float) countSentences(a) / (float)  countWords(a) * 100;
+
+	int index = 0.0588 * L - 0.296 * S - 15.8;
+
+	if(index < 1) {
+		printf("Before 1 grade\n");
+	} else if (index >= 16) {
+		printf("16+ grade\n");
+	} else {
+		printf("Grade: %i\n", index);
+	}
 }
